c13168d9cc465d52aff3c774b079fc5f12ed525a
xoyimi_jingdao <xoyimi@foxmail.com>
2020-04-13 18:07:11 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Detail from "../views/Detail";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
   const routes = [
   {
@@ -10,20 +11,15 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    {
+      path:'/detail',
+      name:'Detail',
+      component:Detail
     }
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
---
+++ src/views/Detail.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="detail">
+        <div class="name">{{name}}</div>
+        <div class="date">{{date}}</div>
+    </div>
+</template>
+
+<script>
+    import html2canvas from 'html2canvas'
+
+    export default {
+        name: "Detail",
+        data() {
+            return {
+                name: '',
+                date: '',
+                time:''
+            }
+        },
+        mounted() {
+            this.name = this.$route.params.name
+            this.date = this.$route.params.date
+            this.time = new Date().toLocaleDateString()
+            this.time=/this.time/
+            console.log(this.time)
+
+        }
+    }
+</script>
+
+<style scoped>
+    .detail {
+        height: 100%;
+        background-image: url("../assets/images/bg2.jpg");
+        background-size: 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+
+    }
+    .name{
+        position: absolute;
+        top: 182px;
+        left: 128px;
+        font-size: 21px;
+        font-weight:700;
+        color:  #89593a;
+        border-bottom: 5px solid #89593a;
+        letter-spacing:6px;
+        text-align: center;
+    }
+</style>
