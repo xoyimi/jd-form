3124e449bfd435c8d05a42dbbf45c45abba455d9
xoyimi_jingdao <xoyimi@foxmail.com>
2020-04-16 10:39:08 +0800

    add wx-jdk solve audio autopaly

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <div id="app">
-    <audio autoplay preload="auto" >
-      <source src="../src/assets/images/bgm.mp3">
-    </audio>
-    <router-view/>
-  </div>
+    <div id="app">
+        <audio id="bgmusic" autoplay preload loop>
+            <source src="../src/assets/images/bgm.mp3">
+        </audio>
+        <router-view/>
+    </div>
 </template>
 
+<script>
+    export default {
+        name: "App",
+        created() {
+          console.dir( this.$wx.ready)
+          this.$wx.ready(function () {
+                console.log('$wx.ready')
+                document.getElementById('bgmusic').play();
+            });
+        }
+    };
+</script>
 <style>
-html,body{
-  height: 100%;
-}
-#app {
-  height: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    html, body {
+        height: 100%;
+    }
+
+    #app {
+        height: 100%;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import wx from 'weixin-js-sdk'
 import '../src/assets/css/normalize.css'
 import '../src/assets/css/animate.css'
 Vue.config.productionTip = false
-
+wx.config({
+  // 配置信息, 即使不正确也能使用 wx.ready
+  debug: false,
+  appId: '',
+  timestamp: 1,
+  nonceStr: '',
+  signature: '',
+  jsApiList: []
+});
+Vue.prototype.$wx=wx
 new Vue({
   router,
   store,
