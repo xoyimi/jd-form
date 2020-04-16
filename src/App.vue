63a9043934a2b772626a3d0288e8085420552065
xoyimi_jingdao <xoyimi@foxmail.com>
2020-04-16 18:07:52 +0800

    tode signature

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,41 @@
 </template>
 
 <script>
+    import Vue from "vue";
+    import wx from "weixin-js-sdk";
+
     export default {
         name: "App",
+        data(){
+            imgUrl:require('../static/logo.jpg')
+        },
         created() {
-          console.dir( this.$wx.ready)
-          this.$wx.ready(function () {
-                console.log('$wx.ready')
+                Vue.axios.get('http://lealai.cn:8080/api/user').then((response)=>{
+                    wx.config({
+                        // 配置信息, 即使不正确也能使用 wx.ready
+                        debug: false,
+                        appId: 'wx80008f1cc41d8a0d',
+                        timestamp: 2222222,
+                        nonceStr: 'asdasdasdasd',
+                        signature: response.data.signature,
+                        jsApiList: ['updateAppMessageShareData','updateTimelineShareData']
+                    });
+                })
+            this.$wx.ready(() => {
+                console.log(' this.$wx.ready')
+                this.$wx.updateAppMessageShareData({
+                    title: '', // 分享标题
+                    desc: '从新出发 有你财好', // 分享描述
+                    link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: this.imgUrl, // 分享图标
+                    success: function () {
+                        // 设置成功
+                        console.log('success')
+                    },
+                    error:function (error) {
+                            console.log(error)
+                    }
+                })
                 document.getElementById('bgmusic').play();
             });
         }
--- src/main.js
+++ src/main.js
@@ -2,22 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import wx from 'weixin-js-sdk'
 import '../src/assets/css/normalize.css'
 import '../src/assets/css/animate.css'
 Vue.config.productionTip = false
-wx.config({
-  // 配置信息, 即使不正确也能使用 wx.ready
-  debug: false,
-  appId: '',
-  timestamp: 1,
-  nonceStr: '',
-  signature: '',
-  jsApiList: []
-});
+
 Vue.prototype.$wx=wx
+Vue.use(VueAxios,axios);
 new Vue({
   router,
   store,
+  axios,
+  VueAxios,
   render: function (h) { return h(App) }
 }).$mount('#app')
