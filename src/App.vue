aae489204eeab7215f008e8699069706846be557
xoyimi_jingdao <xoyimi@foxmail.com>
2020-04-22 18:10:55 +0800

    FIXME 微信验证

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,57 @@
 </template>
 
 <script>
-    // import Vue from "vue";
     import wx from "weixin-js-sdk";
 
     export default {
         name: "App",
-        created() {
-                Vue.axios.get('http://xoyimi.top/getWxConfig').then((res)=>{
-                  let wxConfig=JSON.parse(res)
-                    wx.config({
-                        // 配置信息, 即使不正确也能使用 wx.ready
-                        debug: false,
-                        appId: 'wx80008f1cc41d8a0d',
-                        timestamp: wxConfig.timestamp,
-                        nonceStr: wxConfig.nonce,
-                        signature: wxConfig.signature,
-                        jsApiList: ['updateAppMessageShareData']
-                    });
-                })
-            this.$wx.ready(() => {
+        data() {
+            return {
+                imgUrl: require('@/assets/images/share-logo.jpg')
+            }
+        },
+        mounted() {
+            this.$axios({
+                method: 'post',
+                url: 'http://xoyimi.top/getWxConfig',
+                data: {url: encodeURIComponent(location.href.split('#')[0])} //向服务端提供授权url参数，并且不需要#后面的部分
+            }).then((res) => {
+                let wxConfig = res.data;
+                console.log(encodeURIComponent(location.href.split('#')[0]))
+                wx.config({
+                    debug: true,
+                    appId: 'wx80008f1cc41d8a0d',
+                    timestamp: wxConfig.timestamp,
+                    nonceStr: wxConfig.noncestr,
+                    signature: wxConfig.signature,
+                    jsApiList: ['checkJsApi', 'updateAppMessageShareData']
+                });
+                wx.error(function(res){
+                    console.log("config错误信息",res)
+                    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+                });
+            })
+            wx.checkJsApi({
+                jsApiList: ['updateAppMessageShareData'],
+                success: function (res) {
+                    console.log(res)
+                }
+            });
+            wx.ready(() => {
                 document.getElementById('bgmusic').play();
-                console.log(' this.$wx.ready')
                 this.$wx.updateAppMessageShareData({
-                    title: '', // 分享标题
-                    desc: '从新出发 有你财好', // 分享描述
-                    link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    title: '从新出发 有你财好', // 分享标题
+                    desc: '这里是描述信息，这里是描述信息', // 分享描述
                     imgUrl: this.imgUrl, // 分享图标
                     success: function () {
-                        // 设置成功
-                        console.log('success')
-                    },
-                    error:function (error) {
-                            console.log(error)
+                        // 用户点击了分享后执行的回调函数
+                        console.log('用户点击了分享后执行的回调函数')
                     }
-                })
+                });
+
             });
-        }
+        },
+
     };
 </script>
 <style>
--- src/main.js
+++ src/main.js
@@ -7,14 +7,17 @@ import VueAxios from 'vue-axios'
 import wx from 'weixin-js-sdk'
 import '../src/assets/css/normalize.css'
 import '../src/assets/css/animate.css'
+import VConsole from 'vconsole/dist/vconsole.min.js' //import vconsole
+let vConsole = new VConsole() //实例化
 Vue.config.productionTip = false
 
 Vue.prototype.$wx=wx
+Vue.prototype.$axios=axios
 Vue.use(VueAxios,axios);
 new Vue({
   router,
   store,
-  axios,
   VueAxios,
+  axios,
   render: function (h) { return h(App) }
 }).$mount('#app')
